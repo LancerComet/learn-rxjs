25fcadb06043c4d0632a02b4195b4020d429d206
LancerComet <chw644@hotmail.com>
2019-04-26 02:07:49 +0800

    + Update.

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,36 @@
 import { fromEvent, Observable } from 'rxjs'
 import { map, scan, throttle, throttleTime } from 'rxjs/operators'
 
-fromEvent(
-  document.querySelector('#button-1') as HTMLButtonElement,
-  'click'
-).pipe(
+const button1 = document.querySelector('#button-1') as HTMLButtonElement
+
+fromEvent(button1,'click')
+  .pipe(
     throttleTime(500),
     map(() => 1),
     scan((prev, clickDelta, index) => clickDelta + prev, 0),
   )
   .subscribe(count => console.log(count))
 
-  const observable = new Observable(subscriber => {
-    let count = 0
-    const exec = () => {
-      subscriber.next(count++)
-      setTimeout(exec, 1000)
-    }
-    exec()
-  })
 
-  console.log('invoke subscribe')
-  observable.subscribe({
-    next (value) {
-      console.log('next:', value)
-    },
-    error (error) {
-      console.error('On error:', error)
-    },
-    complete () {
-      console.log('complete')
-    }
-  })
-  console.log('invoke done')
+const observable = new Observable(subscriber => {
+  let count = 0
+  const exec = () => {
+    subscriber.next(count++)
+    setTimeout(exec, 1000)
+  }
+  exec()
+})
+
+console.log('invoke subscribe')
+observable.subscribe({
+  next (value) {
+    console.log('next:', value)
+  },
+  error (error) {
+    console.error('On error:', error)
+  },
+  complete () {
+    console.log('complete')
+  }
+})
+console.log('invoke done')
